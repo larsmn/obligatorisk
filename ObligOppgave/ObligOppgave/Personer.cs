a2c34c2336ce566f496601728b7701e88da0e325
Lars Magnus Nilsen <47142673+larsmn@users.noreply.github.com>
2019-08-16 10:53:33 +0200

    Add files via upload

---
+++ ObligOppgave/ObligOppgave/Personer.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObligOppgave
+{
+    class Personer
+    {
+        public string firstName;
+        public string lastName;
+        public int yearBorn;
+        public string deathYear;
+        public int id;
+        public Personer father;
+        public Personer mother;
+
+        public Personer(string _firstName, string _lastName, int _yearBorn, string _deathYear, int _id, Personer _father = null, Personer _mother = null)
+        {
+            firstName = _firstName;
+            lastName = _lastName;
+            yearBorn = _yearBorn;
+            deathYear = _deathYear;
+            id = _id;
+            father = _father;
+            mother = _mother;
+        }
+
+
+
+        public void Print()
+        {
+            Console.WriteLine("Navn: " + firstName);
+            Console.WriteLine("Etternavn: " + lastName);
+            Console.WriteLine("Fødselsår: " + yearBorn);
+            Console.WriteLine("Dødsår: " + deathYear);
+            Console.WriteLine("Id: " + id);
+            Console.WriteLine();
+            if (father != null)
+            {
+                Console.WriteLine("Far: " + father.firstName + " ID: " + father.id);
+            }
+
+            if (mother != null)
+            {
+                Console.WriteLine("Mor: " + mother.firstName + " ID: " + mother.id);
+            }
+            Console.WriteLine();
+        }
+    }
+}
+//Lag klassen Person.En person skal ha fornavn, etternavn, fødselår og dødsår.
+//Det siste skal selvsagt bare ha verdi hvis personen er død.Alle feltene er frivillige.
+//For eksempel skal du kunne legge inn en person selv om du ikke vet fødselsår.
+//Om man ikke har verdi for noen av feltene, skal man likevel ikke få lov å lage et Person-objekt.
+
---
+++ ObligOppgave/ObligOppgave/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ObligOppgave
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Personer kongen = new Personer(_firstName: "Harald", "N/A", 1937, "N/A", 2);
+            Personer ingrid = new Personer("Ingrid Alexandra", "N/A", 2004, "N/A", 7);
+            Personer olav = new Personer("Olav", "N/A", 1903, "1991", 1);
+            Personer mette = new Personer("Mette Marit", "Høiby", 1973, "N/A", 5);
+            Personer marius = new Personer("Marius", "Borg Høiby", 1997, "N/A", 6);
+            Personer sverre = new Personer("Sverre Magnus", "N/A", 2005, "N/A", 8);
+            Personer sonja = new Personer("Sonja", "Haraldsen", 1937, "N/A", 3);
+            Personer haakon = new Personer("Haakon Magnus", "N/A", 1973, "N/A", 4);
+
+            sverre.father = haakon;
+            sverre.mother = mette;
+            ingrid.father = haakon;
+            ingrid.mother = mette;
+            marius.mother = mette;
+            haakon.father = kongen;
+            haakon.mother = sonja;
+            kongen.father = olav;
+
+            List<Personer> kongefamilien = new List<Personer>(8)
+            {
+                olav,
+                kongen,
+                sonja,
+                haakon,
+                mette,
+                marius,
+                ingrid,
+                sverre
+            };
+
+            Show();
+            start:
+            Console.WriteLine();
+            var answer = Console.ReadLine().ToUpper();
+            Console.WriteLine();
+            if (answer == "HJELP")
+            {
+                Console.WriteLine("Liste => lister alle personer med id, fornavn, fødselsår, dødsår og navn og id på mor og far om det finnes registrert.");
+                Console.WriteLine("Id => viser en bestemt person med mor, far og barn. Eksempel id 5");
+            }
+            if (answer == "LISTE")
+            {
+                foreach (var a in kongefamilien)
+                {
+                    a.Print();
+                }
+            }
+            else if (answer.Contains("ID "))
+            {
+                var answerId = answer.Substring(3);
+                var x = Int32.Parse(answerId);
+                for (var i = 0; i < kongefamilien.ToArray().Length; i++)
+                {
+                    if (kongefamilien[i].id.Equals(x))
+                    {
+                        kongefamilien[i].Print();
+                        var familie = kongefamilien[i];
+                        foreach (var a in kongefamilien)
+                        {
+                            if (a.father == familie || a.mother == familie) a.Print();
+                        }
+                    }
+                }
+            }
+
+            goto start;
+        }
+
+        private static void Show()
+        {
+            Console.WriteLine("Velkommen til det kongelige slektstreet!");
+            Console.WriteLine("Dette er kommandoene du kan skrive:");
+            Console.WriteLine("Hjelp");
+            Console.WriteLine("Liste");
+            Console.WriteLine("Vis ID");
+        }
+
+    }
+}
